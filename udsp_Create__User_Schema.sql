1ad328d917a712898d2972499a602c5c2bf97c44
jerrygoyal <1gouravgg@gmail.com>
2015-09-09 19:35:17 +0530

    added create schema for user table

---
+++ udsp_Create__User_Schema.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE `user_data` (
+  `UserAutoId` int(11) NOT NULL AUTO_INCREMENT,
+  `UserId` varchar(45) DEFAULT NULL,
+  `UserName` varchar(45) DEFAULT NULL,
+  `FullName` varchar(45) DEFAULT NULL,
+  `Phone` varchar(45) DEFAULT NULL,
+  `Email` varchar(45) DEFAULT NULL,
+  `City` varchar(45) DEFAULT NULL,
+  `Dob` varchar(45) DEFAULT NULL,
+  `Password` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`UserAutoId`)
+) ENGINE=InnoDB AUTO_INCREMENT=48 DEFAULT CHARSET=utf8;
